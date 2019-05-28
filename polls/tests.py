5c027407f341625b7938c8dd823e9af3374eaa26
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2019-05-28 11:25:50 +0300

    Generic views and model tests.

--- polls/tests.py
+++ polls/tests.py
@@ -1,6 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_past_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is in the past.
+        """
+        time = timezone.now() - datetime.timedelta(hours=2)
+        question = Question(pub_date=time)
+        self.assertIs(question.was_published_recently(), True)
